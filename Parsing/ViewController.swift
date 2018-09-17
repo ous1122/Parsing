ea3c88fe5cdc6e1876e8d2859a22bf1eadd7283c
D7703_24 <d7703_24@d7703-24ui-imac.local>
2018-09-17 15:55:13 +0900

    123

--- Parsing/ViewController.swift
+++ Parsing/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, XMLParserDelegate {
+    
+    var elements:[[String:String]] = []
+    var item:[String:String] = [:]
+    var currentElement = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let path = Bundle.main.url(forResource: "Fruit", withExtension: "xml") {
+            if let myParser = XMLParser(contentsOf: path) {
+                if myParser.parse() {
+                    
+                    
+            print("Parsing Succed!")
+                    print(elements)
+                    
+                    
+        } else  {
+                    
+                    
+                    print("Parsing Failed!") }
+        
+            
+        } else  {
+                
+                
+            print("parser nil")
+            }
+        
+        
+        } else {
+            
+            
+            print("XML file not found")
+       
+        
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI:String?, qualifiedName qName: String?, attributes attributeDict: [String:String] = [:]) {
+        currentElement = elementName
+        print("currentElement = \(currentElement)")
     }
-
-
+    func parser(_ parser:XMLParser, foundCharacters string: String) {
+        let data = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        if !data.isEmpty {
+            item["name"] = data
+            
+        }
+    }
+    func parser(_ parser:XMLParser, didEndElement elementName: String, namespaceURI:String?, qualifiedName qName: String?) {
+        if elementName == "item" {
+            elements.append(item)
+        }
+    }
 }
-
